9ac8865c85d43f2e05b06502332b958651b04c55
prschulz <prschulz88@gmail.com>
2014-12-04 18:41:15 -0800

    adding insert, isLeaf, and search. Tests are passing. I am happy.

--- binary_tree.js
+++ binary_tree.js
@@ -5,14 +5,48 @@ function BinTree(value){
 }
 
 
-BinTree.prototype.insert = function(){
+BinTree.prototype.insert = function(value){
 
-};
+  if (value === this.value) return alert("node exists");
+  // check root for left (true) or right(false)
+  if (value < this.value){
+    //left
+    if (this.left === null) { // check for null
+      this.left = new BinTree(value); //set left if null
+    }
+    else{
+      this.left.insert(value);
+    }
+  }
+  //else for right
+  else {
+    if (this.right === null) {
+      this.right = new BinTree(value); //set right if null
+    }
+    else{
+      this.right.insert(value);
+    }
+  } //end else for right
+  return this; //return the tree at the end
+}; //end insert function
+
+
 BinTree.prototype.isLeaf = function(){
-
+  return this.left === null && this.right === null;
 };
-BinTree.prototype.search = function(){
 
+
+BinTree.prototype.search = function(value){
+  if (this.value === value) {
+    return true;
+  }
+  else if (this.isLeaf()) {
+    return false; //if value at a leaf and not found end of line
+  }
+  else {
+    //ternary for recursive search call
+    return value < this.value ? this.left.search(value) : this.right.search(value);
+  }
 };
 
 
--- spec/binary_tree_spec.js
+++ spec/binary_tree_spec.js
@@ -94,7 +94,7 @@ describe("BinTree", function(){
     var binTree = new BinTree(5);
     binTree.insert(3).insert(1).insert(2).insert(6);
       it("should check to see if a node is a leaf", function(){
-        expect(binTree.isLeaf(6)).toBe(true);
+        expect(binTree.right.isLeaf()).toBe(true);
       });
     });
 
